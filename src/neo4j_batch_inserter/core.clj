c1620aeb69824b35292411403d6499e49a44a5ed
Jennifer Smith <jennifer@home>
2013-01-12 14:17:10 +0000

    initial commit

---
+++ src/neo4j_batch_inserter/core.clj
@@ -0,0 +1,6 @@
+(ns neo4j-batch-inserter.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/neo4j_batch_inserter/core_test.clj
@@ -0,0 +1,7 @@
+(ns neo4j-batch-inserter.core-test
+  (:use clojure.test
+        neo4j-batch-inserter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
