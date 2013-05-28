053a503b497091a0d27c8f647fab5a43e12a8665
Jennifer Smith <jennifer@home>
2013-05-28 08:37:37 +0100

    More stuffs

--- src/neo4j_batch_inserter/core.clj
+++ src/neo4j_batch_inserter/core.clj
@@ -2,13 +2,17 @@
   (:require [neo4j-batch-inserter.util :as util])
   (:import
    (java.io Closeable)
-   (org.neo4j.graphdb DynamicRelationshipType)
+   (org.neo4j.graphdb DynamicRelationshipType )
+   (org.neo4j.kernel EmbeddedGraphDatabase)
    (org.neo4j.unsafe.batchinsert BatchInserters
                                  BatchInserter)
    (org.neo4j.index.lucene.unsafe.batchinsert LuceneBatchInserterIndexProvider)))
 
 (defprotocol NodeInserter
   (insert-node [this node]))
+
+(defprotocol RelationshipInserter
+  (insert-relationship [this from-node to-node properties]))
 
 (defprotocol Indexer
   (get-index [this index-name index-properties]))
@@ -28,6 +32,11 @@
    NodeInserter
    (insert-node [this node]
      (.createNode inserter (util/create-hashmap node)))
+   RelationshipInserter
+   (insert-relationship [this from-node to-node {:keys [type properties] :or {:properties {}}}]
+     (let [rel-type (DynamicRelationshipType/withName type)]
+       (.createRelationship from-node to-node type (util/create-hashmap properties)  ))
+     )
   Indexer
   (get-index [this index-name index-properties]
     (new LuceneIndex
@@ -44,6 +53,21 @@
 (defn create-batch-inserter [path]
   (let [inserter
         (BatchInserters/inserter path)
-        index-inserter (new LuceneBatchInserterIndexProvider inserter)
-        insert-node (fn [& foo])]
+        index-inserter (new LuceneBatchInserterIndexProvider inserter)]
     (new BatchInserterWrapper inserter index-inserter)))
+
+;;========
+
+(defn run-batch [store-dir operations]
+  (with-open [batch-inserter (create-batch-inserter store-dir)]
+    (doseq [operation operations]
+      (operation batch-inserter))))
+
+(defn insert-node-operation [properties]
+  #(insert-node % properties))
+
+(defn insert-relationship-operation [properties from-node-lookup to-node-lookup]
+  (fn [context ] 
+    (let [from-node (from-node-lookup context) to-node (to-node-lookup context)]
+      #(insert-relationship context from-node to-node properties)
+                  )))
