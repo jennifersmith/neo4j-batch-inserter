67ebc09105c7f2b68d5e523cb2fae51fd61d4af0
Jennifer Smith <jennifer@home>
2013-06-15 16:03:41 +0100

    Now a working austen example

--- src/neo4j_batch_inserter/examples/austen.clj
+++ src/neo4j_batch_inserter/examples/austen.clj
@@ -1,32 +1,35 @@
 (ns neo4j-batch-inserter.examples.austen
-  (:use [neo4j-batch-inserter.core] [clojure.algo.monads]))
+  (:use [neo4j-batch-inserter.core]))
 
-(defn read-characters []
-  [
-   {:name "Elizabeth Bennet"}])
+(defn fetch-characters []
+  (map #(assoc {:type "character"} :name %1 :id %2)
+       ["Jane Bennet"
+         "Elizabeth Bennet"
+         "Lydia Bennet"
+         "Charles Bingley"
+         "George Wickham"
+         "Fitzwilliam Darcy"] 
+       [:jb :lb :cb :gw :fd]))
 
-(defn add-node-m [node]
-  (fn [s]
-    (println "I added a node")
-    [node
-     (assoc s node (rand-int 100000))]))
+(defn make-association [from type to]
+  {:from {:id from :type :character} :to {:id to :type :character} :type type})
 
-(defn index-node-m [node]
-  (fn [s]
-    (println "I indexed a node" (get s node))
-    [node s]))
-
-
-(defn add-and-index [node]
-  (with-monad state-m
-    (domonad [add (add-node-m node)
-              state2 (index-node-m node)]
-              state2)))
+(defn fetch-associations []
+  [(make-association :jb :sisterOf :lb)
+   (make-association :jb :loves :cb)
+   (make-association :eb :fallsInLoveWith :fd)
+   (make-association :eb :fancies :gw)
+   (make-association :eb :sisterOf :lb)
+   (make-association :cb :loves :jb)
+   (make-association  :cb :friendsWith :fd)
+   (make-association :fd :fallsInLoveWith :eb)
+   (make-association :fd :friendsWith :cb)])
 
 
-(defn import-data [database-path]
-  (with-open  [inserter (create-batch-inserter database-path)]
-    (let [
-          characters (get-index inserter :characters {:type :exact})
-          new-node-id     (insert-node inserter (first (read-characters)))]
-      (add characters new-node-id (first (read-characters))))))
+(defn -main [database-path]
+  (println "Creating austen graph at: " database-path)
+  (insert-batch 
+   database-path 
+   {:auto-indexing {:type-fn :type :id-fn :id}}
+   {:nodes (fetch-characters) :relationships (fetch-associations)}))
+
