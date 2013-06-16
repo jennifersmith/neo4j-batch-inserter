ebcfd4f329246c598a8b771c5bf3602219d54abe
Jennifer Smith <jennifer@home>
2013-06-16 23:24:16 +0100

    Merged the two other context vars into a context map with batch-inserter

--- src/neo4j_batch_inserter/core.clj
+++ src/neo4j_batch_inserter/core.clj
@@ -12,18 +12,8 @@
 (defn inc-results [batch-results key]
   (swap! batch-results #(merge-with + % {key 1})))
 
-(defn run-batch [store-dir operations]
-  (with-open [batch-inserter (create-batch-inserter store-dir)]
-    (doseq [operation operations]
-      (operation batch-inserter))))
 
-(defn insert-node-operation [batch-results node]
-  (fn [context]
-    (let [ node-id (insert-node context node)]
-      (inc-results batch-results :nodes-inserted)
-       [node-id node])))
 
-;; I am sure there is a better pattern for this shiz
 
 (defn add-identity-properties
   [{:keys [id-fn type-fn] :or {type-fn :type id-fn :id}} node]
@@ -38,33 +28,45 @@
 (def identity-type (identity-property-fn :type))
 (def identity-id (identity-property-fn :id))
 
+;; ==== "operations" ?
+
+(defn insert-node-operation [ node]
+  (fn [{:keys [batch-inserter batch-results]}]
+    (let [ node-id (insert-node batch-inserter node)]
+      (inc-results batch-results :nodes-inserted)
+       [node-id node])))
+
 (defn get-or-create-node-operation [lookup-fn create-fn]
   #(or (lookup-fn %) (create-fn %)))
 
-(defn insert-relationship-operation [batch-results from-node-lookup to-node-lookup properties type]
-  (fn [context]
+(defn insert-relationship-operation [from-node-lookup to-node-lookup properties type]
+  (fn [{:keys [batch-results batch-inserter] :as context}]
     (let [from-node (from-node-lookup context) to-node (to-node-lookup context)
-          result (insert-relationship context from-node to-node properties type)]
+          result (insert-relationship batch-inserter from-node to-node properties type)]
       (inc-results batch-results :relationships-inserted)
       result)))
 
-(defn index-node-operation [batch-results node-map node-id-fn]
-  (fn [context] (let [[node-id properties] (node-id-fn context)
+(defn index-node-operation [node-id-fn]
+  (fn [{:keys [batch-inserter batch-results node-map] :as context}] 
+    (let [[node-id properties] (node-id-fn context)
                       external-id (identity-id properties)
-                      index (get-index context (identity-type properties) {:type :exact})
+                      index (get-index batch-inserter (identity-type properties) {:type :exact})
                       result (add-to-index index node-id {:id external-id})]
                   (swap! node-map #(assoc % external-id node-id))
                   (inc-results batch-results :nodes-indexed)
                   result)))
 
-(defn lookup-node-operation [ batch-results node-map properties]
-  #(let [
-         index (get-index % (identity-type properties) {:type :exact})
-         id (identity-id properties)]
-     (inc-results batch-results :nodes-looked-up)
-     (or
-      (@node-map id)
-      (read-value index :id id))))
+(defn lookup-node-operation [properties]
+  (fn [{:keys [batch-inserter batch-results node-map]}]
+    (let [
+          index (get-index batch-inserter
+                           (identity-type properties)
+                           {:type :exact})
+          id (identity-id properties)]
+      (inc-results batch-results :nodes-looked-up)
+      (or
+       (@node-map id)
+       (read-value index :id id)))))
 
 (defn node-validity [node]
   (cond (nil? (identity-id node)) :invalid
@@ -76,16 +78,16 @@
         :else :valid))
 
 ;; yauch
-(defn if-valid-node-operation [batch-results node next-fn]
-  (fn [context]
+(defn if-valid-node-operation [node next-fn]
+  (fn [{:keys [batch-results] :as context}]
     (if (= :valid (node-validity node))
       (next-fn context)
       (do
         (inc-results batch-results :invalid-nodes)
         nil))))
 
-(defn if-valid-rel-operation [batch-results rel next-fn]
-  (fn [context]
+(defn if-valid-rel-operation [rel next-fn]
+  (fn [{:keys [batch-results] :as context}]
     (if (= :valid (relationship-validity rel))
       (next-fn context)
       (do
@@ -97,23 +99,20 @@
 ;; need to do everything at one time or another not both cos it confusing
 
 (defn create-massive-function-thing-for-nodes
-  [auto-indexing batch-results node-map]
+  [auto-indexing]
   (fn [node] (let [node (add-identity-properties auto-indexing node)]
-               (if-valid-node-operation batch-results node 
+               (if-valid-node-operation node
                                         (get-or-create-node-operation
-                                         (lookup-node-operation batch-results node-map node)
+                                         (lookup-node-operation node)
                                          (index-node-operation
-                                          batch-results node-map
-                                          (insert-node-operation batch-results node)))))))
+                                          (insert-node-operation node)))))))
 
 (defn create-massive-function-thing-for-rels
-  [batch-results node-fn]
+  [node-fn]
   (fn [{:keys [properties from to type] :as rel}]
     (if-valid-rel-operation
-     batch-results
      rel
-     (insert-relationship-operation batch-results
-                                    (node-fn from)
+     (insert-relationship-operation (node-fn from)
                                     (node-fn to)
                                     properties type))))
 
@@ -122,14 +121,15 @@
 (defn insert-batch [store-dir {:keys [auto-indexing] }
                     {:keys [nodes relationships] :or {:nodes [] :relationships []}}]
   (let [
-        ; This is so dodgy - need to maange state better
-        node-map (atom {})
-        batch-results (atom {})
-        node-fn (create-massive-function-thing-for-nodes auto-indexing batch-results node-map)
-        rel-fn (create-massive-function-thing-for-rels batch-results node-fn)
-        node-operations
-        (map node-fn nodes)
+        node-fn (create-massive-function-thing-for-nodes auto-indexing)
+        rel-fn (create-massive-function-thing-for-rels node-fn)
+        node-operations (map node-fn nodes)
         relationship-operations (map rel-fn relationships)]
     ;; todo: this is dreadful - merge both these functions together and rethink
-    (run-batch store-dir (concat node-operations relationship-operations))
-    @batch-results))
+    (with-open [batch-inserter (create-batch-inserter store-dir)]
+      (let [context {:batch-inserter batch-inserter
+                     :node-map (atom {})
+                     :batch-results (atom {})}]
+        (doseq [operation (concat node-operations relationship-operations)]
+          (operation context))
+        @(context :batch-results)))))
